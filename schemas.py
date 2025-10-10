7405a8947ade74bce784c36df8c8999976af46c8
Guilherme Lavezzo <65193596+Godofin@users.noreply.github.com>
2025-10-10 17:53:54 -0300

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,19 +1,20 @@
 from pydantic import BaseModel
-from typing import Optional, List 
+from typing import Optional, List
 
-# Modelo base com os campos comuns a todas as operações
+# Modelo base com os campos comuns, agora incluindo 'year'
 class EventBase(BaseModel):
     image: str
     alt: str
     title: str
     date: str
+    year: str  
     description: str
     buttonText: str
     eventName: str
-    cities: List[str] = [] 
+    cities: List[str] = []
     active_event: bool = True
 
-# Modelo para a criação de um evento (não precisa de id)
+# Modelo para a criação de um evento (herda 'year' de EventBase)
 class EventCreate(EventBase):
     pass
 
@@ -23,15 +24,16 @@ class EventUpdate(BaseModel):
     alt: Optional[str] = None
     title: Optional[str] = None
     date: Optional[str] = None
+    year: Optional[str] = None  
     description: Optional[str] = None
     buttonText: Optional[str] = None
     eventName: Optional[str] = None
     cities: Optional[List[str]] = None
     active_event: Optional[bool] = None
 
-# Modelo completo do evento, para respostas da API (com id)
+# Modelo completo do evento (herda 'year' de EventBase)
 class Event(EventBase):
     id: int
 
     class Config:
-        from_attributes = True 
+        from_attributes = True
